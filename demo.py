8c21ba02bfdcd7b0daf494fbcb372352150b5e49
h5rdly <h5rdly@proton.me>
2026-03-21 16:59:02 +0200

    Update demo.py

--- demo.py
+++ demo.py
@@ -17,15 +17,14 @@ engine.add_source(
 )
 engine.start_all()
 
-
-# Start empty Lonboard WebGL map
+# Start an empty Lonboard WebGL map
 interactive_map = lonboard.Map(
     layers=[], 
     basemap=MaplibreBasemap(style=CartoStyle.DarkMatter), 
     view_state={"longitude": 34.7, "latitude": 31.5, "zoom": 5, "pitch": 45}
 )
 
-stats = pn.pane.Markdown("### 📡 Live Radar: Awaiting signal...", styles={"color": "#39FF14"})
+stats = pn.pane.Markdown("### Awaiting signal...", styles={"color": "#39FF14"})
 
 
 def update_dashboard():
@@ -36,26 +35,26 @@ def update_dashboard():
         df = pl.from_arrow(data["flights"]).drop_nulls(subset=["longitude", "latitude"])
         pa_table = df.to_arrow()
         
-        # Create the GeoArrow Extension Column natively (Zero GeoPandas!)
+        # GeoArrow Extension Column 
         geometry_col = ga.make_point(pa_table["longitude"], pa_table["latitude"])
         pa_table = pa_table.append_column("geometry", geometry_col)
         
-        # Stream the raw GeoArrow binary to the browser GPU
+        # Stream the GeoArrow binary to the browser GPU
         new_layer = lonboard.ScatterplotLayer(
             table=pa_table,
-            get_fill_color=[57, 255, 20, 200], # glowing green
+            get_fill_color=[57, 255, 20, 200], # Fancy glowy Green
             get_radius=4000,
             radius_min_pixels=2
         )
         
         # Overwrite the map layers
         interactive_map.layers = [new_layer]
-        stats.object = f"### 📡 Live Radar: Tracking **{len(df)}** aircraft globally"
+        stats.object = f"### Tracking **{len(df)}** aircraft"
 
-# Fire the engine poll every 5 seconds
+# Poll every 5 seconds
 pn.state.add_periodic_callback(update_dashboard, period=5000)
 
-# RENDER DASHBOARD
+# Render flights view
 pn.Column(
     stats,
     pn.pane.IPyWidget(interactive_map, sizing_mode="stretch_both", min_height=700),
